d1bda7fd7f8550c82bc317fd7601ad764a7dbe13
Vectormike40 <victor.junior40@yahoo.com>
2019-05-05 18:34:25 +0100

    Navbar ex

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Collapse, 
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
 
-export default function Navbar() {
-    return (
-        <nav>
-            
-        </nav>
-    );
+class Navbar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+      return (
+        <div>
+            <Navbar>
+                <NavbarBrand>Blockbuster</NavbarBrand>
+                <NavbarToggle onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink>Trending</NavLink>
+                            <NavLink>New</NavLink>
+                            <NavLink>Most Watched</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+      )
+    }
 }
