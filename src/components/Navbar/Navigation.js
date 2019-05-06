9c2cf55332690ba35a00b008623c29dbda427b2c
Vectormike40 <victor.junior40@yahoo.com>
2019-05-06 15:08:09 +0100

    More Nav Items

--- src/components/Navbar/Navigation.js
+++ src/components/Navbar/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink } from 'reactstrap';
+import SearchBar from '../SearchBar/SearchBar';
 
 export default class Navigation extends React.Component {
   constructor(props) {
@@ -22,15 +22,82 @@ export default class Navigation extends React.Component {
       <div>
         <Navbar color="faded" light>
         <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <NavbarBrand href="/" className="ml-auto">reactstrap</NavbarBrand>
+          <SearchBar href="/" className="ml-auto"/>
           <Collapse isOpen={!this.state.collapsed} navbar>
+            <span className="discover">Discover</span>
             <Nav navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <NavLink href="/components/">Latest</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink href="/">Popular</NavLink>
               </NavItem>
+              <NavItem>
+                <NavLink href="/">Top Rated</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Upcoming</NavLink>
+              </NavItem>
+            </Nav>
+            <Nav navbar>
+                <span className="genres">Genres</span>
+                <NavItem>
+                    <NavLink>Action</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Adventure</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Animation</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Comedy</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Crime</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Documentary</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Drama</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Family</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Fantasy</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>History</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Horror</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Music</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Mystery</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Romance</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Science Fiction</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>TV Movie</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Thriller</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>War</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Western</NavLink>
+                </NavItem>
             </Nav>
           </Collapse>
         </Navbar>
