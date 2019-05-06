09dbd4f9a784a499c4d7ea96a5b96dd76fd63ec3
Vectormike40 <victor.junior40@yahoo.com>
2019-05-06 15:08:30 +0100

    Using styled components

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+const Form = styled.form`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 1px 20px grey;
+  background-color: black;
+  width: ${props => (props.barOpened ? "30rem" : "2rem")};
+  cursor: ${props => (props.barOpened ? "auto" : "pointer")};
+  padding: 2rem;
+  height: 2rem;
+  border-radius: 8rem;
+  transition: all 800ms cubic-bezier(0.645, 1.045, 0.355, 1);
+`;
+
+
+const Input = styled.input `
+    font-size: 14px;
+    background-color: blue;
+
+`;
+
+const Button = styled.button `
+
+`;
+
 function SearchBar() {
     return (
         <div>
