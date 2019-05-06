38fca686d58e1b7368d8d5909a770e2c68dc8dc1
Vectormike40 <victor.junior40@yahoo.com>
2019-05-06 16:12:25 +0100

    More styling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useLayoutEffect, useRef } from 'react';
 
 import styled from 'styled-components'
 
@@ -12,26 +12,68 @@ const Form = styled.form`
   justify-content: center;
   box-shadow: 0 1px 20px grey;
   background-color: black;
-  width: ${props => (props.barOpened ? "30rem" : "2rem")};
-  cursor: ${props => (props.barOpened ? "auto" : "pointer")};
+  width: ${props => (props.searchOpen ? "30rem" : "2rem")};
+  cursor: ${props => (props.searchOpen ? "auto" : "pointer")};
   padding: 2rem;
   height: 2rem;
   border-radius: 8rem;
   transition: all 800ms cubic-bezier(0.645, 1.045, 0.355, 1);
 `;
 
+const Input = styled.input`
+  font-size: 12px;
+  background-color: transparent;
+  width: 100%;
+  margin-left: ${props => (props.searchOpen ? "1rem" : "0rem")};
+  border: none;
+  transition: all 1000ms cubic-bezier(0.645, 0.045, 0.355, 1);
 
-const Input = styled.input `
-    font-size: 14px;
-    background-color: blue;
-
+  &:focus,
+  
+  &::placeholder {
+    color: blue;
+  }
+  
 `;
 
-const Button = styled.button `
-
+const Button = styled.button`
+  line-height: 1;
+  pointer-events: ${props => (props.searchOpen ? "auto" : "none")};
+  cursor: ${props => (props.searchOpen ? "pointer" : "none")};
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: white;
 `;
 
 function SearchBar() {
+
+    const [state, setState] = useState("");
+    const [searchOpen, setSearchOpen] = useState(false);
+    const FormRef = useRef();
+
+
+    // When user clicks outside of the form, set bar opened to false, to close it
+    const onClick = (e) => {
+        if (formRef.current.contains(e.target)) {
+            // click was inside form, do nothing
+            return;
+        }
+        console.log("Click outside the form, close it");
+        searchOpen(false);
+    };
+
+    useLayoutEffect(() => {
+        // add when mounted
+        document.addEventListener("mousedown", onClick);
+        // cleanup event when unmounted
+        return () => {
+          document.removeEventListener("mousedown", onClick);
+        };
+      }, []);
+
+      
+  
     return (
         <div>
             <h1>search bar</h1>
