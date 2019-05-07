6901cbfa978b2e66c6db0ec4b9229f8082df7ac6
Vectormike40 <victor.junior40@yahoo.com>
2019-05-07 14:52:19 +0100

    SearchBar's smart component.

---
+++ src/containers/Search.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import {} from '../components/SearchBar/SearchBar';
+
+// API key
+const api_key = '779cb993a7038477d49a9deabe16ed77';
+
+
+
+function Search() {
+    async function searchMovie() {
+        
+    }
+}
+
+export default Search;
