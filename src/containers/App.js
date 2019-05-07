5d9939f356892051dc5bcc64c047e22f55757de9
Vectormike40 <victor.junior40@yahoo.com>
2019-05-07 14:51:31 +0100

    ...

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navigation from '../components/Navbar/Navigation';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navigation/>
+    </div>
+  );
+}
+
+export default App;
