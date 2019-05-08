3885ca1bd9302f5e1eb774bf1170576173e07de4
Vectormike40 <victor.junior40@yahoo.com>
2019-05-08 12:58:43 +0100

    ...

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+// Combine reducers allows us combine all of the many reducers we might have
+import { combineReducers } from 'redux';
+
+// Reducer from -->
+
+// Combines reducers and put it into a property to be exported
+export default combineReducers({
+
+});
