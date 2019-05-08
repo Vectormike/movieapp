3503e34259e48b3549ee213d3c7e92189c980ffe
Vectormike40 <victor.junior40@yahoo.com>
2019-05-08 12:59:08 +0100

    Reduce searched movie to come in here

---
+++ src/reducers/moviesReducer.js
@@ -0,0 +1,11 @@
+import { FETCH_MOVIE_SEARCH } from '../action/types';
+
+const initialState = {
+    item: []
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+    }
+}
