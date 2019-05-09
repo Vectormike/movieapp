f7e1aea106a75ad1edc0faca9c15b9d9ad5e7165
Vectormike40 <victor.junior40@yahoo.com>
2019-05-09 16:54:07 +0100

    ...

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import store from '../store/index';
 import '../styles/App.css';
 import Navigation from '../components/Navbar/Navigation';
+import Search from '../containers/Search';
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <Navigation/>
+        <Search/>
       </div>
     </Provider>
   );
