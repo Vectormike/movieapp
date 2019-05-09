d9b7d3489d4518d3ec6e6212277b20a30da6c05d
Vectormike40 <victor.junior40@yahoo.com>
2019-05-09 15:51:44 +0100

    ...

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import store from '../store/index';
 import '../styles/App.css';
 import Navigation from '../components/Navbar/Navigation';
 
