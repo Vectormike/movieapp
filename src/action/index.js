515fd2fd42e333f864d44c1caffd3caac077fe86
Vectormike40 <victor.junior40@yahoo.com>
2019-05-09 16:45:42 +0100

    API fetching and throws 200.

--- src/action/index.js
+++ src/action/index.js
@@ -3,12 +3,14 @@ import API from '../utils/api';
 
 // Fetch movies when searched 
 export const searchMovies =  (query, page) => async dispatch => {
+    console.log('Fetching..');
     const response = await API.get('/search/movie', {
         params: {
             query, 
             page
         }
-    });
+    }); 
+    console.log(response);
     await dispatch({
         types: FETCH_MOVIE_SEARCH,
         payload: response
