ba30b75c473f6cd6f8dbe2d1db2d3d4805b69335
Vectormike40 <victor.junior40@yahoo.com>
2019-05-15 16:29:34 +0100

    ...

--- src/actions.js
+++ src/actions.js
@@ -8,14 +8,30 @@ const api_key = '779cb993a7038477d49a9deabe16ed77';
 
 
 
-console.log('Nothing');
 // Get Popular movies fom api
 export const requestPopular = () => (dispatch) => {
+
+    console.log('Nothing');
     dispatch({type: REQUEST_POPULAR_PENDING});
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}?&language=en-US&page=1`)
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`)
     .then(response => response.json())
     .then(data => dispatch({type: REQUEST_POPULAR_SUCCESS, payload:data}))            
     .catch (error  => dispatch({type: REQUEST_POPULAR_FAILED, payload: error}))
 }
 
 console.log('Nothing');
+
+import API from './utils/api';
+
+// Get Popular movies fom api
+export const requestPopular = () => async dispatch => {
+   try {
+       dispatch({type: REQUEST_POPULAR_PENDING});
+       console.log(API);
+       const response = await API.get('/movie/popular');
+       console.log(response);
+       await dispatch({type: REQUEST_POPULAR_SUCCESS, payload:response})            
+   } catch (error) {
+       dispatch({type: REQUEST_POPULAR_FAILED, payload: error})
+   }
+
