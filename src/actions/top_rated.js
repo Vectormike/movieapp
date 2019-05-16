1fc354030537671a879800f18c2194e42e0a217c
Vectormike40 <victor.junior40@yahoo.com>
2019-05-16 14:49:37 +0100

    Action for top=rated movies

---
+++ src/actions/top_rated.js
@@ -0,0 +1,25 @@
+import {
+    REQUEST_TOP_PENDING,
+    REQUEST_TOP_SUCCESS,
+    REQUEST_TOP_FAILED
+} from './constant';
+
+import API from '../api';
+
+
+// Get Top Rated movies from api
+export const requestTopRated = () => async dispatch => {
+    try {
+        dispatch({type: REQUEST_TOP_PENDING});
+        const response = await API.get('/movie/top_rated');
+        dispatch({
+            type: REQUEST_TOP_SUCCESS,
+            payload: response.data.results
+        })
+    } catch (error) {
+        dispatch({
+            type: REQUEST_TOP_FAILED,
+            payload: error.data.status
+        })
+    }
+}
