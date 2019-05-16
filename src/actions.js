d67a42a84ba73ed6897995f5f2b9852be8d8df66
Vectormike40 <victor.junior40@yahoo.com>
2019-05-16 11:12:11 +0100

    Fixed bug and api is fetched partially

--- src/actions.js
+++ src/actions.js
@@ -4,34 +4,49 @@ import {
     REQUEST_POPULAR_FAILED
 } from './constant';
 
-const api_key = '779cb993a7038477d49a9deabe16ed77'; 
+import API from './api';
+
+// const api_key = '779cb993a7038477d49a9deabe16ed77'; 
 
 
 
-// Get Popular movies fom api
-export const requestPopular = () => (dispatch) => {
-
-    console.log('Nothing');
-    dispatch({type: REQUEST_POPULAR_PENDING});
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`)
-    .then(response => response.json())
-    .then(data => dispatch({type: REQUEST_POPULAR_SUCCESS, payload:data}))            
-    .catch (error  => dispatch({type: REQUEST_POPULAR_FAILED, payload: error}))
-}
+// // Get Popular movies fom api
+// export const requestPopular = () =>  async dispatch => {
+//     try {
+//     dispatch({type: REQUEST_POPULAR_PENDING});
+//     const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`)
+//     const data = res.json()
+//     console.log(data)
+//     await dispatch({
+//         type: REQUEST_POPULAR_SUCCESS,
+//         payload: data
+//     })
+//     } catch (error) {
+//         dispatch({
+//             type: REQUEST_POPULAR_FAILED, 
+//             payload: error.response
+//         })
+//     }
+// }
 
 console.log('Nothing');
-
-import API from './utils/api';
 
 // Get Popular movies fom api
 export const requestPopular = () => async dispatch => {
    try {
        dispatch({type: REQUEST_POPULAR_PENDING});
-       console.log(API);
-       const response = await API.get('/movie/popular');
-       console.log(response);
-       await dispatch({type: REQUEST_POPULAR_SUCCESS, payload:response})            
+       const response = await API.get('/genre/movie/list');
+       dispatch({
+           type: REQUEST_POPULAR_SUCCESS, 
+           payload:response
+        })            
+        console.log(response.data)
    } catch (error) {
-       dispatch({type: REQUEST_POPULAR_FAILED, payload: error})
+       dispatch({
+           type: REQUEST_POPULAR_FAILED, 
+           payload: error.response
+        })
    }
+}
 
+// /genre/movie/list
