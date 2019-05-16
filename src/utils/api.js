5cbbc66f0024e930bbdba5dcabb52878e9d2f549
Vectormike40 <victor.junior40@yahoo.com>
2019-05-16 11:13:04 +0100

    ...

--- src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-
-export default axios.create({
-    baseURL: 'https://api.themoviedb.org/3',
-    params: {
-        api_key: '779cb993a7038477d49a9deabe16ed77' 
-    },
-});
