403a0063075b79adbfccaeca10198a77be6fd8f6
Vectormike40 <victor.junior40@yahoo.com>
2019-05-21 14:48:51 +0100

    Fixed image

--- src/containers/Popular.js
+++ src/containers/Popular.js
@@ -35,15 +35,17 @@ class Popular extends Component {
         popular.map(i => {
           return (
             <div key={i.id} className="pa3 ma">
-            <img  alt='' title='' className="shadow-3"/>
-            <dl className="f7 pa2 ma br3 ">
-                <dt className="clip">Title</dt>        
-                <dd className="ml0 black w-100">{i.title}</dd>
-                <dt className="clip">Description</dt>
-                <dd className="ml0 black w-100">{i.overview}</dd>
-                <dt className="clip">Date</dt>
-                <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
-            </dl>
+              <img src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} alt='img' title='Image' className="shadow-3"/>
+              <dl className="f7 pa2 ma br3 ">
+                  <dt className="clip">Title</dt>        
+                  <dd className="ml0 black w-100">{i.title}</dd>
+                  <dt className="clip">Description</dt>
+                  <dd className="ml0 black w-100">{i.overview}</dd>
+                  <dt className="clip">Vote average</dt>
+                  <dd className="ml0 black w-100">{i.vote_average}</dd>
+                  <dt className="clip">Date</dt>
+                  <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
+              </dl>
           </div>
         )
       })
