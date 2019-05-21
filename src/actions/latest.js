540d2c75ff9326c2a49a6c4b13e903cebd2fd9d6
Vectormike40 <victor.junior40@yahoo.com>
2019-05-21 19:04:04 +0100

    Action for Latest movies

---
+++ src/actions/latest.js
@@ -0,0 +1,23 @@
+import {
+    REQUEST_LATEST_PENDING,
+    REQUEST_LATEST_SUCCESS,
+    REQUEST_LATEST_FAILED 
+} from '../constant';
+
+import API from '../api';
+
+export const requestLatest = () => async dispatch => {
+    try {
+        dispatch({type: REQUEST_LATEST_PENDING});
+        const response = await API.get('/movie/latest');
+        dispatch({
+            type: REQUEST_LATEST_SUCCESS,
+            payload: response.data
+        })
+    } catch (error) {
+        dispatch({
+            type: REQUEST_LATEST_FAILED,
+            payload: error.response.status_message
+        })
+    }
+}
