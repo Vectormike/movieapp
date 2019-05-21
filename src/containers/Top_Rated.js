57220746ea1fb3f3b3e3a2ce8e648be96d4e3a38
Vectormike40 <victor.junior40@yahoo.com>
2019-05-21 14:48:41 +0100

    Fixed image

--- src/containers/Top_Rated.js
+++ src/containers/Top_Rated.js
@@ -25,20 +25,22 @@ class Top_Rated extends Component {
 
     render() {
 		const {top} = this.props;
-        const movies = top.map(i => i.title);
-        const overview = top.map(i => i.overview);
-        const date = top.map(i => i.release_date);
+        // const movies = top.map(i => i.title);
+        // const overview = top.map(i => i.overview);
+        // const date = top.map(i => i.release_date);
         
         return (
             top.map(i => {
                 return (
                   <div key={i.id} className="pa3 ma">
-                  <img  alt='' title='' className="shadow-3"/>
+                  <img src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} alt='img' title='Image' className="shadow-3"/>
                   <dl className="f7 pa2 ma br3 ">
                       <dt className="clip">Title</dt>        
                       <dd className="ml0 black w-100">{i.title}</dd>
                       <dt className="clip">Description</dt>
                       <dd className="ml0 black w-100">{i.overview}</dd>
+                      <dt className="clip">Vote average</dt>
+                      <dd className="ml0 black w-100">{i.vote_average}</dd>
                       <dt className="clip">Date</dt>
                       <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
                   </dl>
