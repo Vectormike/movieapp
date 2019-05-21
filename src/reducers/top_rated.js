aebdf6391dbd465ca5161463cd9ab1009f7609c8
Vectormike40 <victor.junior40@yahoo.com>
2019-05-21 12:04:10 +0100

    fixed tpe mistake

--- src/reducers/top_rated.js
+++ src/reducers/top_rated.js
@@ -1,7 +1,7 @@
 import {
-    REQUEST_POPULAR_PENDING,
-    REQUEST_POPULAR_SUCCESS,
-    REQUEST_POPULAR_FAILED
+    REQUEST_TOP_PENDING,
+    REQUEST_TOP_SUCCESS,
+    REQUEST_TOP_FAILED
 } from '../constant';
 
 const initialTopState = {
@@ -13,11 +13,11 @@ const initialTopState = {
 // Reducer for Top_Rated movies
 export const requestTopRated = (state= initialTopState, action={}) => {
     switch (action.type) {
-        case REQUEST_POPULAR_PENDING:
+        case REQUEST_TOP_PENDING:
             return Object.assign({}, state, {isPending: true});
-        case REQUEST_POPULAR_SUCCESS:
+        case REQUEST_TOP_SUCCESS:
             return Object.assign({}, state, {top: action.payload, isPending: false});
-        case REQUEST_POPULAR_FAILED:
+        case REQUEST_TOP_FAILED:
             return Object.assign({}, state, {error: action.payload, isPending:false})
         default:
             return state;
