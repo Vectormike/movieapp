dae83c454b58e8333d45bb039cee47c20b013570
Vectormike40 <victor.junior40@yahoo.com>
2019-05-21 19:45:30 +0100

    Latest  movies component

---
+++ src/containers/Latest.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { requestPopular } from '../actions';
+import { requestLatest } from '../actions/latest';
+
+
+const mapStateToProps = (state) => {
+  return {
+    latest: state.requestLatest.latest,
+    isPending: state.requestLatest.isPending,
+    error: state.requestLatest.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onRequestLatest: () => dispatch(requestLatest())
+  }
+}
+
+class Latest extends Component {
+
+  componentDidMount() {
+    this.props.onRequestLatest()
+  }
+
+  render() {
+    const { latest } = this.props;
+    // const movies = popular.map(i => i.title);
+    // const overview = popular.map(i => i.overview);
+    // const date = popular.map(i => i.release_date);
+   
+    return (
+        
+        latest.map(i => {
+          return (
+            <div key={i.id} className="pa3 ma">
+              <img src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} alt='img' title='Image' className="shadow-3"/>
+              <dl className="f7 pa2 ma br3 ">
+                  <dt className="clip">Title</dt>        
+                  <dd className="ml0 black w-100">{i.title}</dd>
+                  <dt className="clip">Description</dt>
+                  <dd className="ml0 black w-100">{i.overview}</dd>
+                  <dt className="clip">Vote average</dt>
+                  <dd className="ml0 black w-100">{i.vote_average}</dd>
+                  <dt className="clip">Date</dt>
+                  <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
+              </dl>
+          </div>
+        )
+      })
+    )
+  }
+}
+
+export default connect (mapStateToProps, mapDispatchToProps)(Latest);
