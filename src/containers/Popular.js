cdcf4dadd7ef309e4779e80c00dfb9c5742367ef
Vectormike40 <victor.junior40@yahoo.com>
2019-05-21 11:46:14 +0100

    Mapped movies..

--- src/containers/Popular.js
+++ src/containers/Popular.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { connect } from 'react-redux';
 // import { requestPopular } from '../actions';
 import { requestPopular } from '../actions/popular';
@@ -19,52 +18,7 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-
-const Card = styled.div`
-  background: #fff;
-  border-radius: 15px;
-  font-family: calibri;
-  width: 250px;
-  position: relative;
-  margin: 100px auto 0;
-  text-align: center;
-  box-sizing: border-box;
-  transition: all 1s;
-  cursor: pointer;
-  
-  &:before {
-    content: "";
-    background: transparent url(https://images.unsplash.com/photo-1484611941511-3628849e90f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80)no-repeat center center;
-    background-size: cover;
-    width: 100%;
-    display: block;
-    height: 100%;
-    position: absolute;
-    border-radius: 15px;
-    transition: all 1.4s;
-    left: 0;
-    right: 0;
-    margin: auto;
-    top: 0;
-  }
-  
-  &:hover:before {
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    top: -50px;
-    left: 0;
-    right: 0;
-    margin: auto;
-  } 
-`;
-
-const CardInner = styled.div`
-  padding: 15px;
-`;
-
 class Popular extends Component {
-
 
   componentDidMount() {
     this.props.onRequestPopular()
@@ -72,21 +26,28 @@ class Popular extends Component {
 
   render() {
     const { popular } = this.props;
-    const movies = popular.map(i => i.title);
-    const overview = popular.map(i => i.overview);
-    const date = popular.map(i => i.release_date);
+    // const movies = popular.map(i => i.title);
+    // const overview = popular.map(i => i.overview);
+    // const date = popular.map(i => i.release_date);
    
     return (
-      <div>
-        <Card>
-          <CardInner>
-              <h2>{movies}</h2>
-              <p>{overview}</p>
-              <p>{date}</p>
-          </CardInner>
-        </Card>
-      </div>
-    );
+        
+        popular.map(i => {
+          return (
+            <div key={i.id} className="pa3 ma">
+            <img  alt='' title='' className="shadow-3"/>
+            <dl className="f7 pa2 ma br3 ">
+                <dt className="clip">Title</dt>        
+                <dd className="ml0 black w-100">{i.title}</dd>
+                <dt className="clip">Description</dt>
+                <dd className="ml0 black w-100">{i.overview}</dd>
+                <dt className="clip">Date</dt>
+                <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
+            </dl>
+          </div>
+        )
+      })
+    )
   }
 }
 
