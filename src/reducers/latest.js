1d2a2859b6b990957e98d004e5beb58867aba5b8
Vectormike40 <victor.junior40@yahoo.com>
2019-05-22 12:38:29 +0100

    ...

--- src/reducers/latest.js
+++ src/reducers/latest.js
@@ -17,7 +17,7 @@ export const requestLatest = (state=initialLatestState, action={}) => {
         case REQUEST_LATEST_PENDING:
             return Object.assign({}, state, {isPending: true})
         case REQUEST_LATEST_SUCCESS:
-            return Object.assign({}, state, {latest: action.payload, isPending: false})
+            return Object.assign({}, state, {latest: action.payload, isPending:false})
         case REQUEST_LATEST_FAILED:
             return Object.assign({}, state, {error: action.payload, isPending: false})
         default:
