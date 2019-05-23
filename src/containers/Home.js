3f233f2d75cdae83a9bce9ac71fe15c7daf6e57b
Vectormike40 <victor.junior40@yahoo.com>
2019-05-23 13:13:45 +0100

    Added rows

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { requestTheatres } from '../actions/theatres';
 import { connect } from 'react-redux';
+import '../styles/Home.css';
 
 
 const mapStateToProps = (state) => {
@@ -31,31 +32,27 @@ class Home extends Component {
       <div>
         <h1>In Cinemas now</h1>
 
-        {
-          theatres.map(i => {
-            return (
-              <div className="container">
-                <div className="row">
-                  <div key={i.id} className="col-xs-12 col-md-6 col-lg-3 ">
-                    <img src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} alt='img' title='Image' className="shadow-3"/>
-                    <dl className="f7 pa2 ma br3 ">
-                        <dt className="clip">Title</dt>        
-                        <dd className="ml0 black w-100">{i.title}</dd>
-                        <dt className="clip">Description</dt>
-                        <dd className="ml0 black w-100">{i.overview}</dd>
-                        <dt className="clip">Vote average</dt>
-                        <dd className="ml0 black w-100">{i.vote_average}</dd>
-                        <dt className="clip">Date</dt>
-                        <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
-                    </dl>
-                  </div>
-                </div>
-              </div>
-              
-            )
-          })
-        }
-        
+        <div className="container">
+          <div className="row">
+              {
+                theatres.map(i => {
+                  return (
+                    <div key={i.id} className="col-xs-12 col-md-6 col-lg-3 ">
+                      <div className="card">
+                        <img className="card-img" src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} alt='img' title='Image' className="shadow-3"/>
+                          <h4 className="card-title">{i.title}</h4>
+                          <p className="card-text">{i.overview}</p>
+                          <p>{i.vote_average}</p>
+                          <div className="footer">
+                            <small>Created {i.release_date}</small>
+                          </div>
+                        </div>
+                    </div>                    
+                  )
+                })
+              }      
+          </div>
+        </div>
       </div>
     );
   }
