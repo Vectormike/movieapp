5c5b1e78f3e74137849106869e560872d327cc98
Vectormike40 <victor.junior40@yahoo.com>
2019-05-23 13:14:03 +0100

    Added rows

--- src/containers/Top_Rated.js
+++ src/containers/Top_Rated.js
@@ -30,24 +30,35 @@ class Top_Rated extends Component {
         // const date = top.map(i => i.release_date);
         
         return (
-            top.map(i => {
-                return (
-                  <div key={i.id} className="pa3 ma">
-                  <img src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} alt='img' title='Image' className="shadow-3"/>
-                  <dl className="f7 pa2 ma br3 ">
-                      <dt className="clip">Title</dt>        
-                      <dd className="ml0 black w-100">{i.title}</dd>
-                      <dt className="clip">Description</dt>
-                      <dd className="ml0 black w-100">{i.overview}</dd>
-                      <dt className="clip">Vote average</dt>
-                      <dd className="ml0 black w-100">{i.vote_average}</dd>
-                      <dt className="clip">Date</dt>
-                      <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
-                  </dl>
-                </div>
-              )
-            })
-        );
+            <div>
+            <h1>Most Rated Movies</h1>
+    
+            <div className="container">
+              <div className="row">
+                  {
+                    top.map(i => {
+                      return (
+                            <div key={i.id} className="col-xs-12 col-md-6 col-lg-3">
+                              <img src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} alt='img' title='Image' className="shadow-3"/>
+                              <dl className="f7 pa2 ma br3 ">
+                                  <dt className="clip">Title</dt>        
+                                  <dd className="ml0 black w-100">{i.title}</dd>
+                                  <dt className="clip">Description</dt>
+                                  <dd className="ml0 black w-100">{i.overview}</dd>
+                                  <dt className="clip">Vote average</dt>
+                                  <dd className="ml0 black w-100">{i.vote_average}</dd>
+                                  <dt className="clip">Date</dt>
+                                  <dd className="ml0 black w-100">{i.release_date} <i className="fas fa-heart"></i> </dd>
+                              </dl>
+                            </div>
+                        
+                      )
+                    })
+                  }      
+              </div>
+            </div>
+          </div>
+            );
     }
 }
 
