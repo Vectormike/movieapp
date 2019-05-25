0f763b0032469f3199ee11c75bcad3aa653cd437
Vectormike40 <victor.junior40@yahoo.com>
2019-05-25 21:45:10 +0100

    ...

--- src/reducers/searched.js
+++ src/reducers/searched.js
@@ -14,8 +14,11 @@ export const requestSearched = (state=initialSearch, action={}) => {
     switch (action.type) {
         case REQUEST_SEARCHED_PENDING:
             return Object.assign({}, state, {isPending: true})
-    
+        case REQUEST_SEARCHED_SUCCESS:
+            return Object.assign({}, state, {searchedMovies: action.payload})
+        case REQUEST_SEARCHED_FAILED:
+            return Object.assign({}, state, {error: action.payload})
         default:
-            break;
+            return state
     }
 }
