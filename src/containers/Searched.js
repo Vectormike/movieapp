4f96c51ca4e2415fb1ea6d781e84e7df65a16a31
Vectormike40 <victor.junior40@yahoo.com>
2019-05-27 16:57:58 +0100

    Happy Birthday to  Me

---
+++ src/containers/Searched.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Searched extends Component {
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Searched;
