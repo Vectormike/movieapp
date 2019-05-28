fde3b7d5c4d0247d3a84d7ab63f07eab99b32e14
Vectormike40 <victor.junior40@yahoo.com>
2019-05-28 16:43:08 +0100

    ...

--- src/containers/Searched.js
+++ src/containers/Searched.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
-
+import '../styles/Searched.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 class Searched extends Component {
     
     render() {
         return (
             <div>
+                <div className="fr ma4 wrapper">
+                    <input
+                        id="search"
+                        type="search"
+                        placeholder="Search movie..."
+                    />
+                    <FontAwesomeIcon icon={faSearch} size="1x" />
                 
+                </div>
             </div>
         )
     }
