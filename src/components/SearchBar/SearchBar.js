ec46990dd02607a40691357ec17013248071c490
Vectormike40 <victor.junior40@yahoo.com>
2019-05-28 16:42:43 +0100

    ...

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 import { connect } from 'react-redux';
 import { request } from '../actions/searched';
 
