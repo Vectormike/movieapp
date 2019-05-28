23eaff4c42aabc42a59f26095500f71ec4efc3d1
Vectormike40 <victor.junior40@yahoo.com>
2019-05-28 09:45:34 +0100

    ...

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 import Navigation from '../components/Navbar/Navigation';
 import SearchField from '../containers/Searched';
 import Home from '../containers/Home';
 import Popular from '../containers/Popular';
-
 import TopRated from '../containers/Top_Rated';
 import Latest from './Latest';
+import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Navigation/>
-      <SearchField/>
-      <div className="down">
-        <Switch>
-            <Route exact path ="/" component={Home}/>
-            <Route path="/popular" component={Popular}/>
-            <Route path="/topRated" component={TopRated}/>
-            <Route path="/latest" component={Latest}/>
-        </Switch>
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="tc">
+        <Navigation/>
+        <SearchField/>
+          <Switch>
+              <Route exact path ="/" component={Home}/>
+              <Route path="/popular" component={Popular}/>
+              <Route path="/topRated" component={TopRated}/>
+              <Route path="/latest" component={Latest}/>
+          </Switch>
       </div>
-    </div>
-      
-  );
+        
+    );
+  }
 }
 
 export default App;
